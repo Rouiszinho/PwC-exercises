1510a37d0ff9f80c6edf2a3dac2616d5a7a5d36c
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-05 17:10:53 -0300

    creating ex1.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Code by Luiz / Rouiszinho
+#   
+# ENUNCIADO:
+# Reverta a ordem das palavras nas frases, mantendo a ordem das palavras:
+
+entrada = input("Input: ") # Pedindo a entrada do usuário 
+guardaEntrada = entrada.split() # Separando as palavras 
+print (guardaEntrada)
