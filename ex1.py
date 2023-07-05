f12575190188ac4b9f7e8bbe4d5edcf439194e59
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-05 17:20:35 -0300

    Listing words from last to first position

--- ex1.py
+++ ex1.py
@@ -4,5 +4,5 @@
 # Reverta a ordem das palavras nas frases, mantendo a ordem das palavras:
 
 entrada = input("Input: ") # Pedindo a entrada do usuário 
-guardaEntrada = entrada.split() # Separando as palavras 
+guardaEntrada = list(reversed(entrada.split())) # Listando as palavras da ultima posição à primeira
 print (guardaEntrada)
