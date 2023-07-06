b8866f77d345b32a03d477433582452103615ed0
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-05 21:47:26 -0300

    storing the entry

--- ex2.py
+++ ex2.py
@@ -1,4 +1,11 @@
 # Code by Luiz / Rouiszinho
-#   
+#
 # ENUNCIADO:
 # Remova todos os caracteres duplicados da string abaixo:
+
+entrada = input("Input: ")                    # Pedindo a entrada do usuário
+guardaCaractere = []
+
+for caractere in entrada:                    
+
+    print("Output:", caractere)                  # Imprimindo a frase
