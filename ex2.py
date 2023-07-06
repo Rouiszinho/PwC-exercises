940c390ddf2cd809f6b7550c91d4a482baa16efd
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-05 21:48:47 -0300

    checking if there are no repeated characters

--- ex2.py
+++ ex2.py
@@ -6,6 +6,9 @@
 entrada = input("Input: ")                    # Pedindo a entrada do usuário
 guardaCaractere = []
 
-for caractere in entrada:                    
+for caractere in entrada:                     # Checando se não há caracteres repetidos
+    if caractere not in guardaCaractere:
+        guardaCaractere.append(caractere)
 
-    print("Output:", caractere)                  # Imprimindo a frase
+
+print("Output:", guardaCaractere)                  # Imprimindo a frase
