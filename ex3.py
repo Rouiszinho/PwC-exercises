fab0395e1fdd1854a565b9119991dedd70dfa49d
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-06 13:16:18 -0300

    Detecting if there are letters with accents

--- ex3.py
+++ ex3.py
@@ -4,9 +4,14 @@
 # Encontre a substring palindroma mais longa na string abaixo:
 
 import re
+import unicodedata
 
 entrada = input("Input: ")
+normalizado = unicodedata.normalize('NFD', entrada)
 texto = re.compile(r'[a-zA-Z]')
-correspondencia = texto.finditer(entrada.lower())
+correspondencia = texto.finditer(normalizado.lower())
+tras =''.join(reversed(normalizado.lower()))
+
 for i in correspondencia:
-    print(i)
+    print(i.group())
+print(tras)
