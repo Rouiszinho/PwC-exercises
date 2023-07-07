46c298f2a621df2aebd58ac67ba10c8c78c1caa9
Luiz Henrique Almeida <99974328+Rouiszinho@users.noreply.github.com>
2023-07-07 20:34:55 -0300

    Adding comments to codes

--- ex5.py
+++ ex5.py
@@ -9,7 +9,7 @@ import unicodedata
 def normalizacao(frase):                                # Obtendo somente os caracteres de texto 
     normalizado = unicodedata.normalize('NFD', frase)   # Normaliza a frase removendo acentos
     texto = re.compile(r'[a-z]')                        # Define uma expressão regular para encontrar somente caracteres de texto
-    correspondencia = list(texto.finditer(normalizado.lower()))  # Encontra as correspondências na frase
+    correspondencia = list(texto.finditer(normalizado.lower())) # Encontra as correspondências na frase
     juncao = ""
     for i in correspondencia:
         juncao += i.group()                             # Concatena as letras
